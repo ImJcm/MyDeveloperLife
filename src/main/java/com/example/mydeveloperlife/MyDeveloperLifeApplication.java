6070e746c7d0b340bf4417199cfa30c1b179be1a
ImJcm <wjsckdals0909@naver.com>
2023-06-30 14:03:29 +0900

    init commit

---
+++ src/main/java/com/example/mydeveloperlife/MyDeveloperLifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydeveloperlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDeveloperLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDeveloperLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mydeveloperlife/MyDeveloperLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydeveloperlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDeveloperLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
