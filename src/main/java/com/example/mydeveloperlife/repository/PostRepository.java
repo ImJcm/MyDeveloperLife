a30fdec2ec547958675651308243cc8ddc185d70
ImJcm <wjsckdals0909@naver.com>
2023-06-30 20:04:30 +0900

    postRepository 추가

--- src/main/java/com/example/mydeveloperlife/repository/PostRepository.java
+++ src/main/java/com/example/mydeveloperlife/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.example.mydeveloperlife.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post,Long> {
+//public interface PostRepository extends JpaRepository<Post,Long> {
+public interface PostRepository {
 
 }
