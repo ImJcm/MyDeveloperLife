fcc37e1c348b128c4ad143ed5927f0c48ce9d361
ImJcm <wjsckdals0909@naver.com>
2023-06-30 20:53:42 +0900

    Entity 구성 및 PostService,Repository 추가

---
+++ src/main/java/com/example/mydeveloperlife/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.mydeveloperlife.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column()
+    private String introduce;
+}
