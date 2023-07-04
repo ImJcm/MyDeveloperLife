aa723dce29864a9ead7f7c98baa73802628b8c39
chae0 <limchae098@gmail.com>
2023-07-04 19:00:51 +0900

    FEATURE : add create comment and update comment. also delete comment function.
    1. when the user update or delete comment, check the comment in DB; Is it in DataBase Table.

---
+++ src/main/java/com/example/thedeveloperlife/dto/CommentResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.thedeveloperlife.dto;
+
+import com.example.thedeveloperlife.entity.Comment;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CommentResponseDto {
+
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private String userNickname;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.content = comment.getContent();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
+        this.userNickname = comment.getUser().getNickname();
+    }
+
+}
