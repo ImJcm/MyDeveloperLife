ea3af6460c620ac7caed2d6bb0dc01ee61f9f444
ImJcm <wjsckdals0909@naver.com>
2023-07-05 17:37:02 +0900

    FEATURE : Modify SecruityFilter

--- src/main/java/com/example/thedeveloperlife/config/WebSecurityConfig.java
+++ src/main/java/com/example/thedeveloperlife/config/WebSecurityConfig.java
@@ -69,11 +69,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/user/**").permitAll() // 로그인, 회원가입 누구나 가능.
                         .requestMatchers("/api/categories").permitAll() //전체 카테고리 조회는 누구나 허용
                         .requestMatchers("/api/posts/**").permitAll()  //전체 게시글 조회는 누구나 허용
-                        .requestMatchers("/api/post/write").permitAll()
                         .requestMatchers("/api/post/{id}").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
-//                        .requestMatchers("/api/posts/**").permitAll() // '/api/posts/'로 시작하는 요청 모두 접근 허가 (전체,선택 게시글 조회)
-//                        .requestMatchers("/api/post/**").authenticated() // '/api/posts/'로 시작하는 요청은 인증된 사용자 모두 접근 허가 (게시글,댓글 수정삭제)
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
